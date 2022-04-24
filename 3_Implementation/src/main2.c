94eff5cc36ce4bad3c6069ec9df1d6cb6b2161be
SIVAPRAKASHK3 <sivaprakash.k.2018.eee@rajalakshmi.edu.in>
2022-04-24 16:11:40 +0530

    .

--- 3_Implementation/src/main2.c
+++ 3_Implementation/src/main2.c
@@ -1,17 +1,25 @@
 /*
  */
-
+void CONVNT(unsigned char value);
 #include <avr/io.h>
-
+unsigned char DATA;
 int main(void)
 {
-
+DDRB |=(1<<PB1);
     // Insert code
-DDRD&=~(1<<PD1);
-DDRD|=(1<<PD4);
-PORTD|=(1<<PD1);
+DDRD&=~(1<<PD1);//CLEAR PD1
+DDRD|=(1<<PD4);//SET LED
+DDRC|=(1<<PC0);//TEMPERATURE INPUT
+PORTD|=(1<<PD1);//set PD1 1
+ADCSRA=0x87;
+
+ADMUX=0xE0;
     while(1)
   {
+      ADCSRA |=(1<<ADSC);
+      while(ADCSRA &(1<<ADIF)==0);
+      DATA=ADCH;
+      CONVNT(DATA);
 
       if(!(PIND&(1<<PD1))){
     PORTD|=1<<(PD4);
@@ -22,4 +30,21 @@ else{
   }
 
     return 0;
+}
+
+void CONVNT(unsigned char value)
+{
+    unsigned char x,d1,d2;
+    x=value/10;
+    d1=x;
+    d2=value%10;
+    if(((d1*10)+d2)>35)
+    {
+        PORTB|=(1<<PB1);
+
+    }
+    else{
+        PORTB &=~ (1<<PB1);
+
+    }
 }
